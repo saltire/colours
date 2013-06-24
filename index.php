bb070c8bb5503b856b43463d8d3cd486bcc285c4
saltire sable <saltiresable@gmail.com>
2013-06-24 16:50:49 -0400

    Refactored initial table drawing a bit
    Now the row is defined in one place, whether or not a data file was found.

--- index.php
+++ index.php
@@ -25,39 +25,24 @@
 				<th></th>
 			</tr>
 <?php
+$colours = json_decode(file_get_contents('colours.json'), 1)
+	?: array(0 => array('name' => '',
+		'red' => '', 'green' => '', 'blue' => '',
+		'hue' => '', 'sat' => '', 'val' => ''
+	));
 
-$colours = json_decode(file_get_contents('colours.json'), 1);
-
-if (!$colours) {
-?>
-			<tr>
-				<td class="colour"></td>
-				<td class="sep"></td>
-				<td><input type="text" name="name0" maxlength="50" class="name" value="" /></td>
-				<td class="sep"></td>
-				<td><input type="text" name="hex0" maxlength="7" class="hex" value="" /></td>
-				<td class="sep"></td>
-				<td><input type="text" name="r0" maxlength="3" class="number r" value="" /></td>
-				<td><input type="text" name="g0" maxlength="3" class="number g" value="" /></td>
-				<td><input type="text" name="b0" maxlength="3" class="number b" value="" /></td>
-				<td class="sep"></td>
-				<td><input type="text" name="h0" maxlength="3" class="number h" value="" /></td>
-				<td><input type="text" name="s0" maxlength="3" class="number s" value="" /></td>
-				<td><input type="text" name="v0" maxlength="3" class="number v" value="" /></td>
-				<td class="sep"></td>
-				<td class="colour"></td>
-			</tr>
-<?php
-} else {
-
-	foreach ($colours as $i => $colour) {
+foreach ($colours as $i => $colour) {
+	if ($colour['red'] != '') {
 		$hex = '#' . sprintf('%06x',
 			$colour['red'] * 65536 +
 			$colour['green'] * 256 +
 			$colour['blue']);
+	} else {
+		$hex = '';
+	}
 ?>
 			<tr>
-				<td class="colour" style="visibility: visible; background-color: <?php echo $hex; ?>;"></td>
+				<td class="colour"<?php echo $hex ? " style=\"visibility: visible; background-color: $hex;\"" : ''; ?>></td>
 				<td class="sep"></td>
 				<td><input type="text" name="name<?php echo $i; ?>" maxlength="50" class="name" value="<?php echo $colour['name']; ?>" /></td>
 				<td class="sep"></td>
@@ -71,12 +56,9 @@ if (!$colours) {
 				<td><input type="text" name="s<?php echo $i; ?>" maxlength="3" class="number s" value="<?php echo $colour['sat']; ?>" /></td>
 				<td><input type="text" name="v<?php echo $i; ?>" maxlength="3" class="number v" value="<?php echo $colour['val']; ?>" /></td>
 				<td class="sep"></td>
-				<td class="colour" style="visibility: visible; background-color: <?php echo $hex; ?>;"></td>
+				<td class="colour"<?php echo $hex ? " style=\"visibility: visible; background-color: $hex;\"" : ''; ?>></td>
 			</tr>
-<?php
-	}
-}
-?>
+<?php } ?>
 		</table>
 		<div class="buttons">
 			<a class="add active">&#xff0b; Add</a>
